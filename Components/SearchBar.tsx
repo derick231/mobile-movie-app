7f2da8a7956267a33fc6505eac3f82af854aa8bb
derick <salvationderick23@gmail.com>
2026-01-15 13:01:58 +0545

    designed home screen ui

---
+++ Components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  onPress?: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className="flex-row bg-dark-200 items-center gap-2 rounded-full ">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#ab8bff"
+        className="flex-1 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,105 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, ImageBackground, Text, View } from "react-native";
+
+const TabIcon = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex-row flex-1 mt-4 rounded-full overflow-hidden gap-2 w-full min-w-[115px] min-h-[58px] justify-center items-center "
+      >
+        <Image source={icon} className="size-5" tintColor="#151312" />
+        <Text className="text-secondary text-base font-semibold">{title}</Text>
+      </ImageBackground>
+    );
+  }
+
+  return (
+    <View className="size-full items-center justify-center mt-4">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+};
 
 const _layout = () => {
   return (
-    <Tabs>
-      
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0d23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0d23",
+        },
+      }}
+    >
       <Tabs.Screen
-        name='index'
+        name="index"
         options={{
-          title: "home",
-          headerShown: false
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabIcon focused={focused} icon={icons.home} title="Home" />
+            </>
+          ),
         }}
       />
-        
-    </Tabs>
-  )
-}
 
-export default _layout
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabIcon focused={focused} icon={icons.search} title="Search" />
+            </>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabIcon focused={focused} icon={icons.save} title="Saved" />
+            </>
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <TabIcon focused={focused} icon={icons.person} title="Profile" />
+            </>
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default _layout;
